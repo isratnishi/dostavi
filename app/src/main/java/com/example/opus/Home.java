6c8e13d1b89dbee212d05ad893d821622864c0ed
shoukhin93 <shoukhin1993@gmail.com>
2018-08-13 18:30:14 +0600

    icon added for home activity

--- app/src/main/java/com/example/opus/Home.java
+++ app/src/main/java/com/example/opus/Home.java
@@ -4,23 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
     private LinearLayout requisitionEntryLinearLayout;
     private ImageView requisitionEntryImageView;
+    private TextView emailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        emailTextView = findViewById(R.id.person_email_textview);
         requisitionEntryLinearLayout = findViewById(R.id.requisition_entry_linear_layout);
         requisitionEntryImageView = findViewById(R.id.requisition_entry_image_button);
+
+        emailTextView.setText(Constants.USER_EMAIL);
+
 
         requisitionEntryImageView.setOnClickListener(new View.OnClickListener() {
             @Override
