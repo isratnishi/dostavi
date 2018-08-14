7a416781cd8fd9289dbea07a1c9b51c44d6521c3
shoukhin93 <shoukhin1993@gmail.com>
2018-08-14 16:15:30 +0600

    integrated web data with android in pra approved activity 3

--- app/src/main/java/com/example/opus/RequisitionApproveActivity3.java
+++ app/src/main/java/com/example/opus/RequisitionApproveActivity3.java
@@ -2,20 +2,56 @@ package com.example.opus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.example.opus.Adapters.ApprovedPraAdapter;
+import com.example.opus.Models.ApprovedPraHistoryModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class RequisitionApproveActivity3 extends AppCompatActivity {
+
+    private ArrayList<ApprovedPraHistoryModel> items = new ArrayList<>();
+    RecyclerView recyclerView;
+    ApprovedPraAdapter praAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requisition_approve3);
-        getPraApprovedHistory();;
+        initializeVariables();
+        getPraApprovedHistory();
+    }
+
+    private void initializeVariables() {
+        recyclerView = findViewById(R.id.approved_pra_history_recycler_view);
+        praAdapter = new ApprovedPraAdapter(items, this);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(praAdapter);
+
+       /* ApprovedPraHistoryModel model = new ApprovedPraHistoryModel();
+        model.setDate("a");
+        model.setNextProcessBy("b");
+        model.setProcessBy("c");
+        model.setRemark("asd");
+
+        items.add(model);
+
+        praAdapter.notifyDataSetChanged();*/
+
+
     }
 
     private void getPraApprovedHistory() {
@@ -31,49 +67,30 @@ public class RequisitionApproveActivity3 extends AppCompatActivity {
 
                         Log.d(Constants.LOGTAG, response);
                         //loadStatusProgressbar.setVisibility(View.GONE);
-                      /*  try {
+                        try {
                             JSONArray jsonArray = new JSONArray(response);
 
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject tempObject = jsonArray.getJSONObject(i);
 
-                                String tempItemName = tempObject.getString("ItemName");
-                                String tempItemCode = tempObject.getString("ItemCode");
-                                String tempItemUnit = tempObject.getString("UnitName");
-                                String tempItemSpac = tempObject.getString("ItemSpac");
-                                String tempCumQty = tempObject.getString("TotalProcureQty");
-                                String tempReqQty = tempObject.getString("ReqQTY");
-                                String tempApproxCost = tempObject.getString("ApproxCost");
-                                String tempLastRate = tempObject.getString("LastRate");
+                                String tempProcessBy = tempObject.getString("ProcesName");
+                                String tempDate = tempObject.getString("AprvDate");
+                                String tempRemark = tempObject.getString("Remarks");
+                                String tempNextProcessBy = tempObject.getString("ApproveName");
 
-                                double tempTotal = 0;
-                                try {
-                                    tempTotal = Double.parseDouble(tempCumQty) * Double.parseDouble(tempApproxCost);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-
-
-                                ItemModel itemModel = new ItemModel();
-                                itemModel.setItemName(tempItemName);
-                                itemModel.setItemCode(tempItemCode);
-                                itemModel.setUnit(tempItemUnit);
-                                itemModel.setApproxPrice(tempApproxCost);
-                                itemModel.setSpecification(tempItemSpac);
-                                itemModel.setQuantity(Double.parseDouble(tempCumQty));
-                                itemModel.setLastRate(tempLastRate);
-                                itemModel.setTotal(String.valueOf(String.valueOf(tempTotal)));
-
-                                items.add(itemModel);
+                                ApprovedPraHistoryModel historyModel = new ApprovedPraHistoryModel();
+                                historyModel.setProcessBy(tempProcessBy);
+                                historyModel.setDate(tempDate);
+                                historyModel.setRemark(tempRemark);
+                                historyModel.setProcessBy(tempNextProcessBy);
+                                items.add(historyModel);
 
                             }
-                            itemAdapter.notifyDataSetChanged();
-
-                            //Log.d(Constants.LOGTAG, response);
+                            praAdapter.notifyDataSetChanged();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
-                        }*/
+                        }
                     }
                 },
                 new Response.ErrorListener() {
