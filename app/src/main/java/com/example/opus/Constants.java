bafd04e88d3e830274be73240d4fa6d861517966
shoukhin93 <shoukhin1993@gmail.com>
2018-08-14 12:26:25 +0600

    requisition approval data getting from get api

--- app/src/main/java/com/example/opus/Constants.java
+++ app/src/main/java/com/example/opus/Constants.java
@@ -3,6 +3,9 @@ package com.example.opus;
 public class Constants {
     private static final String BASE_SERVER_ADDRESS = "http://192.168.0.50/OAK/api/OAKAPI/";
     //private static final String BASE_SERVER_ADDRESS = "http://119.148.9.154/OAK/api/OAKAPI/";
+    public static final String LOGIN_URL = "http://192.168.0.50/OAK/Account/LoginAPI";
+    //public static final String LOGIN_URL = "http://119.148.9.154/OAK/Account/LoginAPI";
+
     public static String USER_EMAIL = "";
     public static final String LOGTAG = "logtag";
     public static final String REQUEST_TAG = "com.example.opus";
@@ -14,12 +17,10 @@ public class Constants {
     public static final String POST_REQUISITION_STATUS_LOG = BASE_SERVER_ADDRESS + "SaveRequisitionStatusLog";
     public static final String GET_ALL_LIST = BASE_SERVER_ADDRESS +
             "GETITEMLIST";
+    public static final String GET_PRA_APPROVER = BASE_SERVER_ADDRESS + "GetPraApprover";
 
     public static final String GET_REQUISITION_APPROVAL_LIST = BASE_SERVER_ADDRESS +
             "GetRequisitionApprovalData";
-
-    public static final String LOGIN_URL = "http://192.168.0.50/OAK/Account/LoginAPI";
-    //public static final String LOGIN_URL = "http://119.148.9.154/OAK/Account/LoginAPI";
 
     public static final String REQUISITION_MODEL = "requisitionmodel";
     public static final String REQUISITION_MASTER = "requisitionmastermodel";
--- app/src/main/java/com/example/opus/RequisitionApproveActivity.java
+++ app/src/main/java/com/example/opus/RequisitionApproveActivity.java
@@ -2,8 +2,6 @@ package com.example.opus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -14,6 +12,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,6 +27,9 @@ public class RequisitionApproveActivity extends AppCompatActivity {
     EditText subjectEditText;
     CheckBox compititionWaiverCheckBox;
     EditText supplierNameEditText;
+    EditText approverNameEditText;
+    EditText departmentEditRText;
+    EditText maxAmountEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +50,17 @@ public class RequisitionApproveActivity extends AppCompatActivity {
         subjectEditText = findViewById(R.id.subject_edit_text);
         compititionWaiverCheckBox = findViewById(R.id.compitition_waiver_checkbox);
         supplierNameEditText = findViewById(R.id.supplier_name_edit_text);
+        approverNameEditText = findViewById(R.id.approver_name_edit_text);
+        departmentEditRText = findViewById(R.id.department_edit_text);
+        maxAmountEditText = findViewById(R.id.max_amount_edit_text);
     }
 
 
     private void getJSON() {
         loadStatusProgressbar.setVisibility(View.VISIBLE);
         String id = "146257";
-        String finalURL = Constants.GET_REQUISITION_APPROVAL_LIST + "?id=" + id;
+        String username = "mamun@bnb.com";
+        String finalURL = Constants.GET_PRA_APPROVER + "?username=" + username + "&projectID=" + id;
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET,
                 finalURL,
@@ -62,74 +68,43 @@ public class RequisitionApproveActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         loadStatusProgressbar.setVisibility(View.GONE);
+                        //getPraApprover();
                         //Log.d(Constants.LOGTAG, response);
                         try {
+                            //String object1 = new JSONObject("NextApprover");
+                            // JSONArray jsonArray = new JSONArray(response);
                             JSONObject jsonObject = new JSONObject(response);
-                            String requisitionBy = jsonObject.getString("EmpCode") + "- " +
-                                    jsonObject.getString("EmpName");
-                            String compititionWaiver = jsonObject.getString("CompWaiver");
-                            /*next = findViewById(R.id.next_button);
-                            supplierNameEditText = findViewById(R.id.supplier_name_edit_text);*/
+                            JSONObject requisitionMaster = jsonObject.getJSONObject("RequisitionMaster");
 
-                            Log.d(Constants.LOGTAG, compititionWaiver);
+                            String requisitionBy = requisitionMaster.getString("EmpCode") + "- " +
+                                    requisitionMaster.getString("EmpName");
+                            String compititionWaiver = requisitionMaster.getString("CompWaiver");
 
-                            requisitionNumberEditText.setText(jsonObject.getString("ReqNo"));
-                            requisitionDateEditText.setText(jsonObject.getString("ReqDate"));
-                            productTargetDateEditText.setText(jsonObject.getString("TargetDate"));
+                            requisitionNumberEditText.setText(requisitionMaster.getString("ReqNo"));
+                            requisitionDateEditText.setText(requisitionMaster.getString("ReqDate"));
+                            productTargetDateEditText.setText(requisitionMaster.getString("TargetDate"));
                             requisitionByEditText.setText(requisitionBy);
-                            justificationEditText.setText(jsonObject.getString("Remarks"));
-                            subjectEditText.setText(jsonObject.getString("Subject"));
+                            justificationEditText.setText(requisitionMaster.getString("Remarks"));
+                            subjectEditText.setText(requisitionMaster.getString("Subject"));
 
                             if (!compititionWaiver.equals("null")) {
                                 compititionWaiverCheckBox.setChecked(true);
-                                //subjectEditText.setText(jsonObject.getString(""));
                             }
+
+                            JSONObject nextApprover = jsonObject.getJSONObject("NextApprover");
+                            String approverName = nextApprover.getString("EmpName");
+                            String department = nextApprover.getString("Department");
+                            String maxAmount = nextApprover.getString("MaxAmountPO");
+
+                            approverNameEditText.setText(approverName);
+                            departmentEditRText.setText(department);
+                            maxAmountEditText.setText(maxAmount);
+
 
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        /*try {
-                            JSONArray jsonArray = new JSONArray(response);
-
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject temp = jsonArray.getJSONObject(i);
-
-                                String codeWithName = temp.getString(ShowItemModel.CODE_WITH_NAME);
-                                String id = temp.getString(ShowItemModel.JSON_ID);
-                                String itemCatID = temp.getString(ShowItemModel.ITEM_CAT_ID);
-                                String itemCatName = temp.getString(ShowItemModel.ITEM_CAT_NAME);
-                                String itemCode = temp.getString(ShowItemModel.ITEM_CODE);
-                                String itemDescription = temp.getString(ShowItemModel.ITEM_DESCRIPTION);
-                                String itemName = temp.getString(ShowItemModel.ITEM_NAME);
-                                String itemSpac = temp.getString(ShowItemModel.ITEM_SPAC);
-                                String mainCatName = temp.getString(ShowItemModel.MAIN_CAT_NAME);
-                                String mCatID = temp.getString(ShowItemModel.M_CAT_ID);
-                                String reorderLevel = temp.getString(ShowItemModel.REORDER_LEVEL);
-                                String subCatID = temp.getString(ShowItemModel.SUB_CAT_ID);
-                                String subCatName = temp.getString(ShowItemModel.SUB_CAT_NAME);
-                                String unitName = temp.getString(ShowItemModel.UNIT_NAME);
-
-                                ShowItemModel showItemModel = new ShowItemModel();
-                                showItemModel.setCodeWithName(codeWithName);
-                                showItemModel.setID(id);
-                                showItemModel.setItemCatID(itemCatID);
-                                showItemModel.setItemCatName(itemCatName);
-                                showItemModel.setItemCode(itemCode);
-                                showItemModel.setItemDescription(itemDescription);
-                                showItemModel.setItemName(itemName);
-                                showItemModel.setItemSpac(itemSpac);
-                                showItemModel.setMainCatName(mainCatName);
-                                showItemModel.setmCatID(mCatID);
-                                showItemModel.setReOrderLevel(reorderLevel);
-                                showItemModel.setSubCatID(subCatID);
-                                showItemModel.setSubCatName(subCatName);
-                                showItemModel.setUnitName(unitName);
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }*/
                     }
                 },
                 new Response.ErrorListener() {
@@ -139,5 +114,43 @@ public class RequisitionApproveActivity extends AppCompatActivity {
                 });
         AppSingleton.getInstance(getApplicationContext())
                 .addToRequestQueue(stringRequest, Constants.REQUEST_TAG);
+    }
+
+    private void getPraApprover() {
+        loadStatusProgressbar.setVisibility(View.VISIBLE);
+        String username = "mamun@bnb.com";
+        String finalURL = Constants.GET_PRA_APPROVER + "?username=" + username;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                finalURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        loadStatusProgressbar.setVisibility(View.GONE);
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+                            String approverName = jsonObject.getString("EmpName");
+                            String department = jsonObject.getString("Department");
+                            String maxAmount = jsonObject.getString("MaxAmountPO");
+
+                            approverNameEditText.setText(approverName);
+                            departmentEditRText.setText(department);
+                            maxAmountEditText.setText(maxAmount);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+        AppSingleton.getInstance(getApplicationContext())
+                .addToRequestQueue(stringRequest, Constants.REQUEST_TAG);
+
     }
 }
